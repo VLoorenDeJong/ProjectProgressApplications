fb8382f4a0a77ac8386addbeadb7f5922f253e66
Victor Looren de Jong <contact@victorloorendejong.nl>
2022-01-20 15:23:07 +0100

    Removed generic logic class from progress app

--- ProjectProgressLibrary/Validation/DataValidation.cs
+++ ProjectProgressLibrary/Validation/DataValidation.cs
@@ -64,5 +64,80 @@ namespace ProjectProgressLibrary.Validation
 
             return output;
         }
+
+        public static bool ValidateIfProjectTitleExsists(this string projectTitle, List<ProjectModel> allProjects)
+        {
+
+            bool output = true;
+
+            if (string.IsNullOrEmpty(projectTitle) == true)
+            {
+                return false;
+                // return output;
+            }
+
+            ProjectModel project;
+            try
+            {
+                project = allProjects.Where(x => x.Title.ToLower() == projectTitle.ToLower()).First();
+
+                //project = allProjects.Where(x => x.Title.ToLower().Contains(projectTitle.ToLower())).First();
+            }
+            catch (System.InvalidOperationException)
+            {
+                output = false;
+            }
+
+            return output;
+        }
+        public static bool ValidateIfSearchedProjectTitleExsists(this string projectTitle, List<ProjectModel> allProjects)
+        {
+
+            bool output = true;
+
+            if (string.IsNullOrEmpty(projectTitle) == true)
+            {
+                return false;
+                // return output;
+            }
+
+            ProjectModel project;
+            try
+            {
+                //project = allProjects.Where(x => x.Title.ToLower() == projectTitle.ToLower()).First();
+
+                project = allProjects.Where(x => x.Title.ToLower().Contains(projectTitle.ToLower())).First();
+            }
+            catch (System.InvalidOperationException)
+            {
+                output = false;
+            }
+
+            return output;
+        }
+        public static bool ValidateIfItemExists(this string itemToSearch, List<string> allItemKeys)
+        {
+            bool output = true;
+
+            if (string.IsNullOrEmpty(itemToSearch) == true)
+            {
+                return false;
+            }
+
+            string item = null;
+            try
+            {
+                item = allItemKeys.Where(x => x.ToLower().Contains(itemToSearch.ToLower())).First();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return output;
+
+
+        }
+       
     }
 }
