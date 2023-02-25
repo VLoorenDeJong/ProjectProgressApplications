c8e33e7083563e9df25c8932c2e5116374ffdf6d
Victor Looren de Jong <contact@victorloorendejong.nl>
2023-02-25 18:45:51 +0100

    HTTPControllerAdded

---
+++ ProgressAppAPI1/Controllers/HTTPController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProgressAppAPI1.Controllers
+{   
+    [Route("[controller]")]
+    [ApiController]
+    public class HTTPController : ControllerBase
+    {
+        [HttpGet("GetIp", Name = "GetIp")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<string> GetIpAddress()
+        {
+            string returnString = "HelloFromTheAPI";
+
+            HttpContext context = HttpContext;
+            if (context is not null)
+            {
+
+            }
+
+            return Ok(returnString);
+        }
+    }
+
+}
